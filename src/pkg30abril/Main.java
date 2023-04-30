c0b7423c9a56287b98bc181e67541f259867f59f
Ricardo8118 <rikrdo8118@gmail.com>
2023-04-30 11:58:01 +0100

    Primer commit 30abril

---
+++ src/pkg30abril/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg30abril;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la edad");
+        int edad;
+        edad =sc.nextInt();
+        String letras;
+        
+        
+        System.out.println("la edad es "+edad);
+        
+        
+        // TODO code application logic here
+    }
+    
+}
